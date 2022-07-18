55ca38ee70eeecad5cb5ed0043b946020e96c53f
Suraksha <surakshak571@gmail.com>
2022-07-18 19:24:18 +0500

    Api consume

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -30,4 +30,69 @@ router.post("/register", async(req,res) => {
 
 })
 
+router.get("/getdata",async(req,res)=>{
+    try {
+        const userdata = await users.find();
+        res.status(201).json(userdata)
+        console.log(userdata);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+
+// get individual user
+
+router.get("/getuser/:id",async(req,res)=>{
+    try {
+        console.log(req.params);
+        const {id} = req.params;
+
+        const userindividual = await users.findById({_id:id});
+        console.log(userindividual);
+        res.status(201).json(userindividual)
+
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+
+
+// update user data
+
+router.patch("/updateuser/:id",async(req,res)=>{
+    try {
+        const {id} = req.params;
+
+        const updateduser = await users.findByIdAndUpdate(id,req.body,{
+            new:true
+        });
+
+        console.log(updateduser);
+        res.status(201).json(updateduser);
+
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+
+
+// delete user
+router.delete("/deleteuser/:id",async(req,res)=>{
+    try {
+        const {id} = req.params;
+
+        const deletuser = await users.findByIdAndDelete({_id:id})
+        console.log(deletuser);
+        res.status(201).json(deletuser);
+
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+
+
 module.exports = router; 
