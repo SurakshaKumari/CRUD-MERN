e3c9c92ba36e95a97c510db0648feb5d39e156c6
Suraksha <surakshak571@gmail.com>
2022-07-18 19:34:03 +0500

    navbar added

--- suraksha/src/component/navbar.js
+++ suraksha/src/component/navbar.js
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <header className='mb-5'>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+   <NavLink to={'/'}><a className="navbar-brand" href="#">Home</a></NavLink> 
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+         <NavLink to="/register"> <a className="nav-link active" aria-current="page" href="#">Register</a> </NavLink>
         </li>
        
        
